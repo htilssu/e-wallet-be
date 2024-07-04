69022f56433ab422aaab3b7f43b728aba14328f3
htilssu <tolashuu@gmail.com>
2024-07-05 06:20:30 +0700

    feat: otp

--- src/main/java/com/ewallet/ewallet/model/User.java
+++ src/main/java/com/ewallet/ewallet/model/User.java
@@ -1,9 +1,5 @@
 package com.ewallet.ewallet.model;
 
 public class User {
-    private double money;
-    private String gender;
-    private String address;
-    private String phone;
-    private String role;
+
 }
